c4fb0952e9b8a1395145a210c92df01358f87440
gokhan <gokhancakicioglu@mantis.com.tr>
2021-10-17 23:13:21 +0300

    products added

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -3,15 +3,18 @@ import { popularProducts } from "../data"
 import Product from "./Product"
 
 const Container = styled.div`
-
+    padding: 20px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
 `
 
 const Products = () => {
     return (
         <Container>
-            {popularProducts.map(item => {
-                <Product item={item} />
-            })}
+            {popularProducts.map((item) => (
+                <Product item={item} key={item.id}/>
+            ))}
         </Container>
     )
 }
